9b3f766dee73dfc2ab71779a44dded74d6450732
Matt Dudzik <mdudzik92@gmail.com>
2021-06-13 23:33:47 -0500

    Altered HTML and JS a bit to clean it up, starting over a bit removing bootstrap

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var startButton = document.querySelector("#startButton");
-var answerChoices = document.getElementById("answers");
+// Variables to connect to DOM elements
+var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("timer");
+var answerChoices = document.getElementById("answers");
+var startBtn = document.querySelector("#startButton");
 var questionSpace = document.getElementById("question-space");
-var secondsLeft = 90;
-var questionNumber = -1;
+
+// Variables for setting the state of the quiz
 var currentQuestionIndex = 0;
-var answer;
+var time = questions.length * 15;
+var timerId;
 
 var questions = [
 	{
@@ -31,60 +34,90 @@ var questions = [
 	},
 ];
 
-function questionBuilder() {
-	// click the 'start the game' button to hide the button
-	document.querySelector(".start-quiz").style = "display: none";
-	// after the button is hid, the screen changes to the first question and multiple choice answers
-	document.querySelector(".question-area").textContent =
-		questions[currentQuestionIndex].Q;
-	for (var i = 0; i < questions[currentQuestionIndex].Choices.length; i++) {
-		var choicesArea = document.querySelector(".answers-area");
-		// create a button
-		var button = document.createElement("button");
-		// add text for the current choice
-		button.textContent = questions[currentQuestionIndex].Choices[i];
-		// add data value showing the choices index to see if they clicked on the correct answer
-		button.setAttribute("data-index", i);
-		// add that button to the page
-		button.setAttribute("class", "choiceButton");
-		choicesArea.appendChild(button);
-		button.addEventListener("click", handleAnswer);
+function startGame() {
+	// Click the button to hide the start screen
+	var startScreenEl = document.getElementById("start-screen");
+	startScreenEl.setAttribute("class", "hide");
+
+	// Show the questions section
+	questionsEl.removeAttribute("class");
+
+	// Starts the timer
+	timerId = setInterval(startTimer, 1000);
+
+	// Shows starting time
+	timerEl.textContent = time;
+
+	// Then runs the nextQuestion function, starting the quiz off. User chooses an answer and submits, taking them to the next question/answer choices, repeats until done
+	nextQuestion();
+}
+
+function startTimer() {
+	time--;
+	timerEl.textContent = "Time: " + time;
+
+	if (time <= 0) {
+		quizEnd();
 	}
-	// user chooses an answer and submits, taking them to the next question/answer choices, repeats until done
 }
 
-// function nextQuestion() {
-//     document.querySelector(".question-area").textContent = questions[currentQuestionIndex].Q
-//     for (var i = 0; i < questions[currentQuestionIndex].Choices.length; i++) {
-//         var choicesArea = document.querySelector(".answers-area");
-//         var button = document.createElement("button");
-//         button.textContent = questions[currentQuestionIndex].Choices[i];
-//         button.setAttribute("data-index", i)
-//         // button.setAttribute("class", "choiceButton")
-//         choicesArea.appendChild(button)
-//         button.addEventListener("click", function(){console.log("button clicked")})
-//     }
-// }
+function nextQuestion() {
+	// Getting current question object from array
+	var currentQuestion = questions[currentQuestionIndex];
 
-function handleAnswer() {
-	console.log("handleAnswer");
-	currentQuestionIndex++;
-	questionBuilder();
-	// when the user clicks their choice, it needs to check to see if they clicked the correct one
-	// check current question's "correct" property
-	// compare that to the data-index of the choice that was clicked on
-	// if it was incorrect, decrease their time
-	// go to next question
+	// Updating the title with the current question
+	var titleEl = document.getElementById("question-title");
+	titleEl.textContent = currentQuestion.title;
+
+	// Clearing out old question choices
+	choicesEl.innerHTML = "";
+
+	// Looping over choices
+	currentQuestion.choices.forEach(function (choice, i) {
+		// Creating new button for each choice
+		var choiceNode = document.createElement("button");
+		choiceNode.setAttribute("class", "choice");
+		choiceNode.setAttribute("value", choice);
+
+		choiceNode.textContent = i + 1 + ". " + choice;
+
+		// Attaching click event listener to each choice
+		choiceNode.onclick = questionClick;
+
+		// Display on the page
+		choicesEl.appendChild(choiceNode);
+	});
 }
 
-document
-	.querySelector(".start-quiz")
-	.addEventListener("click", questionBuilder);
-// document.querySelector(".choiceButton").addEventListener("click", nextQuestion)
-
-let secs = 0;
-function countUp() {
-	secs++;
-	console.log(secs);
+function answerButtons() {
+	if (answer === event.target.textContent) {
+		feedbackDisplay.innerHTML = "Correct!";
+	}
 }
-const quizTime = setInterval(countUp, 1000);
+
+function quizEnd() {
+	// Stops timer
+	clearInterval(countdown);
+
+	// Shows end screen
+	var endScreenEl = document.getElementById("end-screen");
+	endScreenEl.removeAttribute("class");
+
+	// Shows final score
+	var finalScoreEl = document.getElementById("final-score");
+	finalScoreEl.textContent = time;
+
+	// Hides questions section
+	document.getElementById("quiz").classList.add("d-none");
+}
+
+function clockTick() {
+	// Update time
+	time--;
+	timerEl.textContent = time;
+
+	// Checking if user ran out of time
+}
+
+// Click the button to start the quiz
+startBtn.onclick = startQuiz;
